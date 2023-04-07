70b5508ac9826db3a7a0bdfa31e67ac7a5b04db7
mantri swetha <mantriswetha98@gmail.com>
2023-04-07 17:09:13 +0000

    slider

---
+++ src/components/PlanetsSlider/index.js
@@ -0,0 +1,25 @@
+// Write your code here
+import Slider from 'react-slick'
+import './index.css'
+import PlanetItem from '../PlanetItem'
+
+const PlanetsSlider = props => {
+  const {planetsList} = props
+  const settings = {
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+
+  return (
+    <div className="background" data-testid="planets">
+      <Slider {...settings}>
+        {planetsList.map(each => (
+          <PlanetItem key={each.id} planetDetails={each} />
+        ))}
+      </Slider>
+    </div>
+  )
+}
+
+export default PlanetsSlider
